347dd8d08fd76beb0dd53052c248ed3d7b1c326e
ccbueno3 <caiocb3@al.insper.edu.br>
2023-04-30 10:24:42 -0300

    Exercício 4 do EP2

---
+++ posiciona_frota.py
@@ -0,0 +1,22 @@
+def posiciona_frota(frota):
+    #tabuleiro vazio
+    tabuleiro = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+    for navios in frota.values():
+        for lista_coordenadas in navios:
+            for coordenadas in lista_coordenadas:
+                x = coordenadas[0]
+                y = coordenadas[1]
+                tabuleiro[x][y] = 1
+    return tabuleiro
+     
