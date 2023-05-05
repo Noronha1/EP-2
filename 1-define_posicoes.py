1b72ca517bc88b5c56c3a44cb04984ce24a5042e
Noronha1 <peedroroberto@gmail.com>
2023-05-05 07:40:17 -0300

    correção no define posicies

--- 1-define_posicoes.py
+++ 1-define_posicoes.py
@@ -1,16 +1,20 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
     if tamanho == 1:
         lista = [[int(linha),int(coluna)]]
-        return lista   
+        
+         
     else:
         if orientacao == "vertical":
             lista = []
             for i in range(tamanho):
-                lista.append([int(linha)+i,int(coluna)])
-            
+                linha2 = linha + i
+                lista.append([int(linha2),int(coluna)])
+
+                
         if orientacao == "horizontal":
             lista = []
             for i in range(tamanho):
-                lista.append([int(linha) ,int(coluna)+ i])
+                coluna2 = coluna + i
+                lista.append([int(linha) ,int(coluna2)])
 
     return lista
